6a85038a715275608d928e23129573ac91f078dd
Adrianzatreanu <zgadi_2007@yahoo.com>
2019-04-18 13:30:08 +0300

    Implement sanity checker
    Fixes https://github.com/Adrianzatreanu/CLI_Tasks/issues/1

---
+++ server/sanitychecker.py
@@ -0,0 +1,29 @@
+import sys
+
+class SanityChecker:
+    OK = 0
+
+    @staticmethod
+    def perform_login_sanity_checks(data):
+        res = ""
+
+        if "username" not in data:
+            res += "username missing."
+
+        if res == "":
+            return SanityChecker.OK
+        return res
+
+    @staticmethod
+    def perform_execute_sanity_checks(data):
+        res = ""
+
+        if "cmd" not in data:
+            res += "cmd missing."
+
+        if "username" not in data:
+            res += "username missing."
+
+        if res == "":
+            return SanityChecker.OK
+        return res
--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS
 import json
 
+from sanitychecker import SanityChecker
+
 app = Flask(__name__)
 CORS(app)
 
@@ -13,8 +15,12 @@ def login():
     if request.method != "POST":
         return json.dumps({"login": "Only POST is supported"})
     data = request.get_json(force=True)
-    username = data["username"]
 
+    sanity_check_result = SanityChecker.perform_login_sanity_checks(data)
+    if sanity_check_result != SanityChecker.OK:
+        return json.dumps({"login": sanity_check_result})
+
+    username = data["username"]
     if not check_login(username):
         return json.dumps({"login": "failed"})
 
@@ -25,6 +31,11 @@ def execute():
     if request.method != "POST":
         return json.dumps({"execute": "Only POST is supported"})
     data = request.get_json(force=True)
+
+    sanity_check_result = SanityChecker.perform_execute_sanity_checks(data)
+    if sanity_check_result != SanityChecker.OK:
+        return json.dumps({"execute": sanity_check_result})
+
     cmd = ' '.join(data["cmd"])
     username = data["username"]
     output = "Command is not supported."
