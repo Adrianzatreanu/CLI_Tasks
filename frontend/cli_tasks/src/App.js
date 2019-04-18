4f6d47e0d08d9059bf92ef3a435bd0754102172c
Adrianzatreanu <zgadi_2007@yahoo.com>
2019-04-18 18:12:01 +0300

    Add basic commands with mock data
    Fixes https://github.com/Adrianzatreanu/CLI_Tasks/issues/4

--- frontend/cli_tasks/src/App.js
+++ frontend/cli_tasks/src/App.js
@@ -9,7 +9,13 @@ class App extends Component {
     this.props = props;
     this.state = {
       username: "guest",
+<<<<<<< HEAD
       msg: "Welcome to CLI Tasks. Please enter your username, or `help` to show a list of helpful commands."
+=======
+      msg: "Welcome to CLI Tasks. Please enter your username, or `help` to show a list of helpful commands.",
+      topic: "none",
+      task: "none"
+>>>>>>> Add basic commands with mock data
     };
   }
 
@@ -49,6 +55,13 @@ class App extends Component {
   }
 
   list_topics(args, print) {
+<<<<<<< HEAD
+=======
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+>>>>>>> Add basic commands with mock data
     console.log("list_topics called");
     axios.get('http://0.0.0.0:8001/list_topics')
       .then(function (response) {
@@ -64,6 +77,7 @@ class App extends Component {
   }
 
   list_tasks(args, print) {
+<<<<<<< HEAD
     console.log("list_tasks called");
   }
 
@@ -74,19 +88,144 @@ class App extends Component {
     }
     var topic = cmd_args[0];
     console.log("change_topic called with topic " + topic);
+=======
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+    console.log("list_tasks called");
+
+    if (!this.topic_selected()) {
+      print("Select a topic first.");
+    }
+
+    axios.post('http://0.0.0.0:8001/list_tasks', {
+        topic: this.state["topic"]
+      })
+      .then(response => {
+        console.log(response);
+        var tasks = response["data"]["list_tasks"];
+        for (var i = 0; i < tasks.length; i++) {
+          print(tasks[i]);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  change_topic(args, print) {
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+    var cmd_args = args["_"];
+    if (cmd_args.length !== 1) {
+      print("Invalid usage. change_topic <topic_name>");
+      return;
+    }
+    var topic = cmd_args[0];
+    console.log("change_topic called with topic " + topic);
+
+    axios.get('http://0.0.0.0:8001/list_topics')
+      .then(response => {
+        console.log(response);
+        var topics = response["data"]["list_topics"];
+        if (topics.includes(topic)) {
+          print("Topic changed successfully.");
+          this.setState({
+            "topic": topic
+          })
+        } else {
+          print("Invalid topic name");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  start_task(args, print) {
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+
+    var cmd_args = args["_"];
+    if (cmd_args.length !== 2) {
+      print("Invalid usage. start_task <task_name>");
+    }
+
+    var task = cmd_args[0];
+    console.log("start_task called with task " + task);
+>>>>>>> Add basic commands with mock data
   }
 
   show_current_topic(args, print) {
     console.log("show_current_topic called");
+<<<<<<< HEAD
+=======
+
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+
+    if (!this.topic_selected()) {
+      print("No topic selected");
+      return;
+    }
+
+    print(this.state["topic"]);
+
+>>>>>>> Add basic commands with mock data
   }
 
   show_score(args, print) {
     console.log("show_score called");
+<<<<<<< HEAD
 
     if (this.state["username"] === "guest") {
       print("Not logged in.");
       return;
     }
+=======
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+  }
+
+  check(args, print) {
+    console.log("check called");
+
+    if (!this.logged_in()) {
+      print("Not logged in.");
+      return;
+    }
+
+    if (!this.topic_selected()) {
+      print("No topic selected");
+      return;
+    }
+
+    if (!this.task_selected()) {
+      print("No task selected.");
+      return;
+    }
+  }
+
+  logged_in() {
+    return this.state["username"] !== "guest";
+  }
+
+  task_selected() {
+    return this.state["task"] !== "none";
+  }
+
+  topic_selected() {
+    return this.state["topic"] !== "none";
+>>>>>>> Add basic commands with mock data
   }
 
   render() {
@@ -121,6 +260,14 @@ class App extends Component {
                 this.change_topic(args, print);
               }
             },
+<<<<<<< HEAD
+=======
+            'start_task': {
+              method: (args, print, runCommand) => {
+                this.start_task(args, print);
+              }
+            },
+>>>>>>> Add basic commands with mock data
             'show_current_topic': {
               method: (args, print, runCommand) => {
                 this.show_current_topic(args, print);
@@ -130,14 +277,30 @@ class App extends Component {
               method: (args, print, runCommand) => {
                 this.show_score(args, print);
               }
+<<<<<<< HEAD
+=======
+            },
+            'check': {
+              method: (args, print, runCommand) => {
+                this.check(args, print);
+              }
+>>>>>>> Add basic commands with mock data
             }
           }}
           descriptions={{
             'list_topics': 'lists all available topics',
             'list_tasks': 'lists tasks for the currently selected topic',
+<<<<<<< HEAD
             'change_topic': 'usage: change_topic <topic_name>. changes to that topic.',
             'show_current_topic': 'shows current active topic',
             'show_score': "shows your current score",
+=======
+            'change_topic': 'usage: change_topic <topic_name>. changes to that topic',
+            'start_task': 'usage: start_task <task_name>. starts that topic',
+            'show_current_topic': 'shows current active topic',
+            'show_score': "shows your current score",
+            'check': "checks if the current task is complete and updates score if needed",
+>>>>>>> Add basic commands with mock data
           }}
           commandPassThrough={(cmd, print) => {
             if (this.state["username"] === "guest") {
